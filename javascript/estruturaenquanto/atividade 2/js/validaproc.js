650789dde370c237b64e63391ef8bea4901d4e90
vitorCCSS <vitor_cesar-silva@estudante.sesisenai.org.br>
2025-08-11 21:06:55 -0300

    Salvando alterações

--- javascript/estruturaenquanto/atividade 2/js/validaproc.js
+++ javascript/estruturaenquanto/atividade 2/js/validaproc.js
@@ -2,14 +2,21 @@ function validaProc()
 { 
     let i=1;
     let soma = 0;
+    let maior = 0
     while(i<=3) //while = enquanto / do while = faça enquanto
     {  
         numero = parseInt(prompt("Digite um numero:"));
         console.log("O numero informado é:"+ numero );
         soma = soma + numero;
         i++;
+        
+        if(numero > maior)
+        {
+            maior = numero
+        }
     }
     console.log("A somatoria dos numeros é "+soma);
+    console.log("O maior numero informado é: "+maior);
     return false;
 
 }
